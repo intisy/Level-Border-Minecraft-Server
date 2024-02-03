9f1548b256ed390f3ec757e77fe05059ed02c824
Pizza <wilderpizza99@gmail.com>
2024-02-03 22:13:58 +0100

    initial commit

---
+++ start.bat
@@ -0,0 +1,11 @@
+:start
+title Bamboo Tycoon Lobby 1
+@echo off
+color 7
+echo +----------------------------------------------------+
+echo :                                                    :
+echo :                     SKYPIXEL                       :
+echo :                                                    :
+echo +----------------------------------------------------+
+java -Dfile.encoding=UTF-8 -Xmx8G -jar spigot.jar
+goto start
